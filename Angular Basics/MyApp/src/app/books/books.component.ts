58ab6d6031fe547136f535e08255eb75a0bdf831
maruf-swe <marufur1847@gmail.com>
2023-12-08 00:51:41 +0600

    property binding in img src

--- Angular Basics/MyApp/src/app/books/books.component.ts
+++ Angular Basics/MyApp/src/app/books/books.component.ts
@@ -9,15 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BooksComponent implements OnInit{
 
+  src : string = 'https://m.media-amazon.com/images/I/51E2055ZGUL._SL1000_.jpg';
+  name : string = 'Clean Code';
+  author : string = 'Robert C Martin';
+
+
+  src2 : string = 'https://m.media-amazon.com/images/I/7167aaVxs3L._SL1500_.jpg';
+  name2 : string = 'Effective Java';
+  author2 : string = 'Joshua Bloch';
+
   constructor(){}
 
   ngOnInit(): void {
     
   }
   
-  src : string = 'https://m.media-amazon.com/images/I/51E2055ZGUL._SL1000_.jpg'
-  name : string = 'Clean Code'
-  author : string = 'Robert C Martin';
 
 
 }
