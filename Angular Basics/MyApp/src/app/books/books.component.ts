30af91873c22555e90b7ce2f16129b1dd4bc53ef
maruf-swe <marufur1847@gmail.com>
2023-12-10 01:17:42 +0600

    add Pipes examples

--- Angular Basics/MyApp/src/app/books/books.component.ts
+++ Angular Basics/MyApp/src/app/books/books.component.ts
@@ -1,4 +1,4 @@
-import { NgFor, NgIf } from '@angular/common';
+import { CurrencyPipe, NgFor, NgIf, TitleCasePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
@@ -6,12 +6,13 @@ interface Book{
   name: string
   author: string
   image_src: string
+  price : number
 }
 
 @Component({
   selector: 'app-books',
   standalone: true,
-  imports: [FormsModule,NgFor,NgIf],
+  imports: [FormsModule,NgFor,NgIf,TitleCasePipe,CurrencyPipe],
   templateUrl: './books.component.html',
   styleUrl: './books.component.css'
 })
@@ -20,23 +21,27 @@ export class BooksComponent implements OnInit{
   books:Book[] = [
     {
       image_src : 'https://m.media-amazon.com/images/I/51E2055ZGUL._SL1000_.jpg',
-      name : 'Clean Code',
-      author : 'Robert C Martin'
+      name : 'clean code',
+      author : 'Robert C Martin',
+      price : 500
     },
     {
       image_src : 'https://m.media-amazon.com/images/I/7167aaVxs3L._SL1500_.jpg',
-      name : 'Effective Java',
-      author : 'Joshua Bloch'
+      name : 'effective java',
+      author : 'Joshua Bloch',
+      price : 1000
     },
     {
       image_src : 'https://m.media-amazon.com/images/I/51E2055ZGUL._SL1000_.jpg',
       name :  'Clean Code',
-      author :  'Robert C Martin'
+      author :  'Robert C Martin',
+      price : 200
     },
     {
       image_src : 'https://m.media-amazon.com/images/I/7167aaVxs3L._SL1500_.jpg',
       name : 'Effective Java',
-      author : 'Joshua Bloch'
+      author : 'Joshua Bloch',
+      price : 300
     }
   ]
 
