00003f892887c2db9c59a23e97fa32c3fd6e3303
maruf-swe <marufur1847@gmail.com>
2023-12-11 00:06:56 +0600

    add book component

--- Angular Basics/MyApp/src/app/app.component.ts
+++ Angular Basics/MyApp/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { BooksComponent } from './books/books.component';
+import { BookComponent } from './book/book.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, BooksComponent],
+  imports: [CommonModule, RouterOutlet, BooksComponent, BookComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
